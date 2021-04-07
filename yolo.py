df7091e7cc57934c3c78bccd1e9959e161743fbe
Rory-Godwin <rgry.godwin@gmail.com>
2021-04-07 17:54:45 +0100

    FOLO1
    Modification
    OUT
     IN  62,66
    MOD 68
    ADDED 77,78

--- yolo.py
+++ yolo.py
@@ -22,7 +22,7 @@ from utils.utils import letterbox_image
 #--------------------------------------------#
 class YOLO(object):
     _defaults = {
-        "model_path"        : 'model_data/yolov4_tiny_weights_voc.h5',
+        "model_path"        : 'model_data/tiny.h5',
         "anchors_path"      : 'model_data/yolo_anchors.txt',
         "classes_path"      : 'model_data/voc_classes.txt',
         "score"             : 0.5,
